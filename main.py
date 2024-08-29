5743c883444b0683d5508a8aeebad94e2fd1ae1a
asdf <guitarL1fe@gmail.com>
2024-08-29 00:37:14 -0600

    game is now centered on screen

--- main.py
+++ main.py
@@ -3,6 +3,21 @@ from game_logic import DataClickerGame
 
 if __name__ == "__main__":
     root = tk.Tk()
-    root.geometry("1200x650")  # Increased height to accommodate the new button
+    
+    # Get screen width and height
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    # Set window size
+    window_width = 1200
+    window_height = 650
+
+    # Calculate position coordinates
+    x = (screen_width - window_width) // 2
+    y = (screen_height - window_height) // 2
+
+    # Set the geometry of the window
+    root.geometry(f"{window_width}x{window_height}+{x}+{y}")
+    
     app = DataClickerGame(root, enable_commands=True)  # Set to False to disable commands
     root.mainloop()
